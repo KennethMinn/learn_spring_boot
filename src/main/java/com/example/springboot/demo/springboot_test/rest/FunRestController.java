39a4d3e05cd3ec83fc18bb313ac6df823f6e4ebb
KennethMinn <minthukha2472005@gmail.com>
2024-06-11 19:20:49 +0630

    initialized

---
+++ src/main/java/com/example/springboot/demo/springboot_test/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.example.springboot.demo.springboot_test.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //annotation for controller
+public class FunRestController {
+    @GetMapping("/") //http get handler route
+    public String sayHello(){
+        return "hello wrold!";
+    }
+}
---
+++ src/test/java/com/example/springboot/demo/springboot_test/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.springboot_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
