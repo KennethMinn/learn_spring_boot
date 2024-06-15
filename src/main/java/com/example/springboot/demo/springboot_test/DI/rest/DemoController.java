05b095b31df266d0cd8a8d010c08a434c1b5f5db
KennethMinn <minthukha2472005@gmail.com>
2024-06-15 10:58:55 +0630

    Setter injection

--- src/main/java/com/example/springboot/demo/springboot_test/DI/rest/DemoController.java
+++ src/main/java/com/example/springboot/demo/springboot_test/DI/rest/DemoController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    private final Coach myCoach;
-    //define a constructor for dependency injection
-    @Autowired //tells spring to inject a dependency
+    private Coach myCoach;
+//    define a constructor for dependency injection
+    @Autowired //tells spring to inject a dependency - constructor injection
     public  DemoController(Coach theCoach){
         this.myCoach=theCoach;
     }
+
+//    @Autowired //tells spring to inject a dependency - setter injection - also works with any method name just add the annotation
+//    public void  setCoach(Coach theCoach){
+//        this.myCoach=theCoach;
+//    }
 
     @GetMapping("/dailyWorkout")
     public String getDailyWorkouot(){
